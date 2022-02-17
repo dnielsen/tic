b13f79daaa96639964d4dc508e0c41f57c1ec529
pat-s <patrick.schratz@gmail.com>
2022-02-17 15:58:25 +0100

    test

--- tests/testthat/test-integration-package-failure.R
+++ tests/testthat/test-integration-package-failure.R
@@ -10,23 +10,25 @@ test_that("integration test: package failure", {
     package_path,
     fields = list(License = "GPL-2"), rstudio = FALSE, open = FALSE
   )
-  withr::with_dir(
-    package_path,
-    { # nolint
-      writeLines("do_package_checks()", "tic.R")
-      writeLines("^tic\\.R$", ".Rbuildignore")
-      dir.create("tests")
-      writeLines('stop("Check failure!")', "tests/test.R")
-      expect_error(
-        callr::r(
-          function() {
-            tic::run_all_stages()
-          },
-          show = TRUE,
-          env = c(callr::rcmd_safe_env(), TIC_LOCAL = "true")
-        ),
-        'A step failed in stage "script"'
-      )
-    }
-  )
+  withr::with_envvar(c("R_USER_CACHE_DIR" = tempfile()), {
+    withr::with_dir(
+      package_path,
+      { # nolint
+        writeLines("do_package_checks()", "tic.R")
+        writeLines("^tic\\.R$", ".Rbuildignore")
+        dir.create("tests")
+        writeLines('stop("Check failure!")', "tests/test.R")
+        expect_error(
+          callr::r(
+            function() {
+              tic::run_all_stages()
+            },
+            show = TRUE,
+            env = c(callr::rcmd_safe_env(), TIC_LOCAL = "true")
+          ),
+          'A step failed in stage "script"'
+        )
+      }
+    )
+  })
 })
